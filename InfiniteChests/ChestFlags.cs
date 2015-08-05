f52f5d740ee9168c8ad3f8249b8537ba06969bd2
Zaicon Kiroshu <kzaicon@yahoo.com>
2015-08-05 16:07:45 -0500

    1.3 Commit

---
+++ InfiniteChests/ChestFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfiniteChests
+{
+	[Flags]
+	public enum ChestFlags
+	{
+		Public = 1,
+		Region = 2,
+		Refill = 4,
+		Bank = 8
+	}
+}
---
+++ InfiniteChests/PlayerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfiniteChests
+{
+	public class PlayerInfo
+	{
+		public ChestAction Action;
+		public int BankID;
+		public int Index;
+		public string Password = "";
+		public int RefillTime;
+		public int X;
+		public int Y;
+		public int TransactionsLeft = 0;
+		public bool ShouldCloseAfterTransactions = false;
+	}
+}
